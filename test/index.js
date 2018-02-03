a5417b09a476ad3bccf9d697e9db1a8a5443f691
chenlei <chenlei@hongkingsystem.com>
2018-02-03 16:05:57 +0800

    update messagetype;

--- test/index.js
+++ test/index.js
@@ -4,8 +4,12 @@ let qtp = new qtpmodule.QtpService();
 qtp.connect(6001, '172.24.13.23');
 qtp.onConnect = () => {
   console.info(`onConnect`);
-  qtp.send(101, JSON.stringify({ data: { user_id: 100101, password: "1223" } }), 10);
+  qtp.send(101, JSON.stringify({ data: { user_id: 19999, password: "2cb6703cc7cb7d564008ddbfaad68eE2" } }), 10);
 };
+
+qtp.onTopic(5002, (key, body)=> {
+  console.info(msg.toString());
+}, this);
 
 qtp.onClose = () => {
   console.info("onClose");
@@ -15,15 +19,16 @@ qtp.addSlot({
   service: 10,
   msgtype: 102,
   callback: (msg) => {
-    qtp.sendToCMS("getProduct", JSON.stringify({ data: { body: { userid: 100101 } } }));
+    // qtp.sendToCMS("getProduct", JSON.stringify({ data: { body: { userid: 100101 } } }));
+    qtp.subscribe(5002, [1114113], false, 50);
   }
 });
 
 qtp.addSlotOfCMS("getProduct", (arg) => {
-  setTimeout(() => {
-    qtp.dispose();
-    qtp.connect(6001, '172.24.13.23');
-  }, 10000);
+  // setTimeout(() => {
+  //   qtp.dispose();
+  //   qtp.connect(6001, '172.24.13.23');
+  // }, 10000);
 }, this);
 
 qtp.addSlot({
