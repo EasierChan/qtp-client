22cb9e8c26eb5691fd4f26f86fac0933269a9c87
chenlei <chenlei@hongkingsystem.com>
2018-03-31 13:53:05 +0800

    fix add issue;

--- test/index.js
+++ test/index.js
@@ -1,7 +1,22 @@
 const qtpmodule = require('../lib/qtp.service');
 
+const net = require("net");
+net.createServer((client)=> {
+  client.on("connect", ()=> {
+    console.info("new connect");
+  });
+
+  client.on("close", ()=> {
+    console.info("close client");
+  });
+
+  setTimeout(()=> {
+    client.end();
+  }, 5000);
+}).listen(8001);
+
 let qtp = new qtpmodule.QtpService();
-qtp.connect(8001, '139.159.224.15');
+qtp.connect(8001, '139.159.224.13');
 qtp.onConnect = (e) => {
   console.info(`onConnect`, e);
   qtp.send(101, JSON.stringify({ data: { user_id: 19999, password: "2cb6703cc7cb7d564008ddbfaad68eE2", ip: "123", mac: "123",
@@ -12,9 +27,12 @@ qtp.onTopic(5002, (key, body)=> {
   console.info(body.toString());
 }, this);
 
-qtp.onClose = () => {
-  console.info("onClose");
+qtp.onClose = (addr) => {
+  console.info("onClose", addr);
+  qtp.connect(8001, '139.159.224.13');
 };
+// qtp.dispose();
+// qtp.connect(8001, '139.159.228.13');
 
 qtp.onError = ()=> {
   console.info("onError");
@@ -26,9 +44,8 @@ qtp.addSlot({
   callback: (msg) => {
     console.info("logined", msg.toString())
     qtp.sendToCMS("getProduct", JSON.stringify({ data: { head: {}, body: { } } }));
-    setTimeout(() => {
+    setTimeout(()=> {
       qtp.dispose();
-      qtp.connect(8001, '139.159.224.13');
     }, 5000);
     // qtp.send(1010, JSON.stringify({"data":{"body":{"acid":1000002},"head":{"reqsn":32,"userid":19999},"serviceid":40,"msgtype":1010}}), 40, 1);
     // qtp.subscribe(5002, [1114113], false, 50);
