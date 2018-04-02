72e7ecc443164f62a554aa71a76b27b15823de52
chenlei <chenlei@hongkingsystem.com>
2018-04-02 12:17:17 +0800

    update tls sock;

--- test/index.js
+++ test/index.js
@@ -1,24 +1,25 @@
 const qtpmodule = require('../lib/qtp.service');
 
-const net = require("net");
-net.createServer((client)=> {
-  client.on("connect", ()=> {
-    console.info("new connect");
-  });
+// const net = require("net");
+// net.createServer((client)=> {
+//   client.on("connect", ()=> {
+//     console.info("new connect");
+//   });
 
-  client.on("close", ()=> {
-    console.info("close client");
-  });
+//   client.on("close", ()=> {
+//     console.info("close client");
+//   });
 
-  setTimeout(()=> {
-    client.end();
-  }, 5000);
-}).listen(8001);
+//   setTimeout(()=> {
+//     client.end();
+//   }, 5000);
+// }).listen(8001);
 
 let qtp = new qtpmodule.QtpService();
 qtp.connect(8001, '139.159.224.13');
 qtp.onConnect = (e) => {
   console.info(`onConnect`, e);
+  
   qtp.send(101, JSON.stringify({ data: { user_id: 19999, password: "2cb6703cc7cb7d564008ddbfaad68eE2", ip: "123", mac: "123",
   app_md5: "89d59beea4dc61b057bdb094fa47969f", app_version: "1.0.3.5", app_name: "chronos-ui"  } }), 10);
 };
@@ -29,13 +30,14 @@ qtp.onTopic(5002, (key, body)=> {
 
 qtp.onClose = (addr) => {
   console.info("onClose", addr);
-  qtp.connect(8001, '139.159.224.13');
 };
 // qtp.dispose();
 // qtp.connect(8001, '139.159.228.13');
 
 qtp.onError = ()=> {
   console.info("onError");
+  qtp.dispose();
+  qtp.connect(8001, '139.159.224.13');
 }
 
 qtp.addSlot({
@@ -43,10 +45,10 @@ qtp.addSlot({
   msgtype: 102,
   callback: (msg) => {
     console.info("logined", msg.toString())
-    qtp.sendToCMS("getProduct", JSON.stringify({ data: { head: {}, body: { } } }));
-    setTimeout(()=> {
-      qtp.dispose();
-    }, 5000);
+    // qtp.sendToCMS("getProduct", JSON.stringify({ data: { head: {}, body: { } } }));
+    // setTimeout(()=> {
+    //   qtp.dispose();
+    // }, 5000);
     // qtp.send(1010, JSON.stringify({"data":{"body":{"acid":1000002},"head":{"reqsn":32,"userid":19999},"serviceid":40,"msgtype":1010}}), 40, 1);
     // qtp.subscribe(5002, [1114113], false, 50);
   }
@@ -60,6 +62,14 @@ qtp.addSlot({
   }
 })
 
+  setTimeout(() => {
+    qtp.dispose();
+    i= 1000;
+    
+    setTimeout(()=> {
+      qtp.connect(8001, '139.159.224.13');
+    }, 1000)
+  }, 5000);
 qtp.addSlotOfCMS("getProduct", (arg) => {
   // setTimeout(() => {
   //   qtp.dispose();
