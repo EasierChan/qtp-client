448721c21f8bde7b714adfbc9fcd3380afcce603
chenlei <chenlei@hongkingsystem.com>
2019-01-02 10:40:37 +0800

    add sendQ ；

--- test/index.js
+++ test/index.js
@@ -1,42 +1,52 @@
-const qtpmodule = require('../lib/qtp.service');
+// import { QtpMessageOption } from "../lib/model/message.model";
 
-// const net = require("net");
-// net.createServer((client)=> {
-//   client.on("connect", ()=> {
-//     console.info("new connect");
-//   });
+const { QtpService, QtpMessageOption } = require('../index');
+const { readFileSync } = require('fs');
 
-//   client.on("close", ()=> {
-//     console.info("close client");
-//   });
+// // const net = require("net");
+// // net.createServer((client)=> {
+// //   client.on("connect", ()=> {
+// //     console.info("new connect");
+// //   });
 
-//   setTimeout(()=> {
-//     client.end();
-//   }, 5000);
-// }).listen(8001);
+// //   client.on("close", ()=> {
+// //     console.info("close client");
+// //   });
 
-let qtp = new qtpmodule.QtpService();
-qtp.connect(8012, '172.24.10.35', false);
+// //   setTimeout(()=> {
+// //     client.end();
+// //   }, 5000);
+// // }).listen(8001);
+
+let qtp = new QtpService();
 qtp.onConnect = (e) => {
   console.info(`onConnect`, e);
-  
-  qtp.send(101, JSON.stringify({ data: { user_id: 19999, password: "2cb6703cc7cb7d564008ddbfaad68eE2", ip: "123", mac: "123",
-  app_md5: "89d59beea4dc61b057bdb094fa47969f", app_version: "1.0.3.5", app_name: "chronos-ui"  } }), 10);
+  let options = [];
+  let userOpt = new QtpMessageOption();
+  userOpt.id = 111;
+  userOpt.value = Buffer.from("1213");
+  options.push(userOpt);
+  // qtp.sendWithOption(101, options, "", 0);
+  qtp.send(101, JSON.stringify({
+    data: {
+      "user_id": 9999, "password": "c356042e4df23f932957ed9b9fa904E0", "dsn": "*", "cpuid": "*", "mac": "507b9dcd5b44", "ip": "172.16.1.36", "app_name": "cms", "app_version": "1.0.9.28", "app_md5": "3732d4516b1328d4fb5141be8fb97473"
+    }
+  }), 10);
 };
 
-qtp.onTopic(5002, (key, body)=> {
-  console.info(body.toString());
-}, this);
+// qtp.onTopic(5002, (key, body)=> {
+//   console.info(body.toString());
+// }, this);
 
-qtp.onClose = (addr) => {
-  console.info("onClose", addr);
-};
-// qtp.dispose();
-// qtp.connect(8001, '139.159.228.13');
+// qtp.onClose = (addr) => {
+//   console.info("onClose", addr);
+// };
+// // qtp.dispose();
+// // qtp.connect(8001, '139.159.228.13');
 
-qtp.onError = ()=> {
-  console.info("onError");
-}
+// qtp.onError = ()=> {
+//   console.info("onError");
+// }
 
 qtp.addSlot({
   service: 10,
@@ -49,36 +59,48 @@ qtp.addSlot({
     // }, 5000);
     // qtp.send(1010, JSON.stringify({"data":{"body":{"acid":1000002},"head":{"reqsn":32,"userid":19999},"serviceid":40,"msgtype":1010}}), 40, 1);
     // qtp.subscribe(5002, [1114113], false, 50);
-  }
-});
+    console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
+    console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
+    console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
+    console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
+    console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
 
-qtp.addSlot({
-  service: 0,
-  msgtype: 100,
-  callback: (msg)=> {
-    console.info(msg.toString());
-  }
-})
-
-  setTimeout(() => {
-    qtp.dispose();
-    i= 1000;
-    
     setTimeout(()=> {
-      qtp.connect(8001, '172.24.13.10', false);
-    }, 1000)
-  }, 5000);
-qtp.addSlotOfCMS("getProduct", (arg) => {
-  // setTimeout(() => {
-  //   qtp.dispose();
-  //   qtp.connect(8001, '127.0.0.1');
-  // }, 5000);
-}, this);
-
-qtp.addSlot({
-  service: 40,
-  msgtype: 1010,
-  callback: (msg) => {
-    console.info(msg.toString());
+      console.info(qtp.sendToCMS("saveTradeAccountPosition", JSON.stringify({ data: { body: { acid: 11111, excelDat: readFileSync("D:\\zDoc\\11111.csv", "utf8") } } })));
+    }, 1000);
   }
 });
+
+qtp.addSlotOfCMS("saveTradeAccountPosition", (msg) => {
+  console.info(msg.toString());
+}, this);
+qtp.connect(8001, '139.159.224.13');
+// qtp.addSlot({
+//   service: 0,
+//   msgtype: 100,
+//   callback: (msg)=> {
+//     console.info(msg.toString());
+//   }
+// })
+
+//   setTimeout(() => {
+//     qtp.dispose();
+
+//     setTimeout(()=> {
+//       qtp.connect(8001, '172.24.13.10', false);
+//     }, 1000)
+//   }, 5000);
+// qtp.addSlotOfCMS("getProduct", (arg) => {
+//   // setTimeout(() => {
+//   //   qtp.dispose();
+//   //   qtp.connect(8001, '127.0.0.1');
+//   // }, 5000);
+// }, this);
+
+// qtp.addSlot({
+//   service: 40,
+//   msgtype: 1010,
+//   callback: (msg) => {
+//     console.info(msg.toString());
+//   }
+// });
